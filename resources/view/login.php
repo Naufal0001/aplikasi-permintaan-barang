c9b94f28a6a7d3df60f30b8c38b668e07f9e1a19
Suryakonelo <Suryaputram06@gmail.com>
2023-07-18 12:00:08 +0700

    index

--- resources/view/login.php
+++ resources/view/login.php
@@ -13,7 +13,7 @@
                 <h2 class="text-2xl font-bold text-center mb-8">Log In</h2>
                 <form method="POST" action="">
                     <div class="mb-4">
-                        <label class="block mb-2 font-semibold" for="email">Email</label>
+                        <label class="block mb-2 font-semibold" for="email">username</label>
                         <input id="email" type="email"
                             class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500"
                             name="email" autocomplete="email" autofocus
